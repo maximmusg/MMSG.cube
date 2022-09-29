f3d20e330f6ca0d0cd0266aefdf491838ab662e5
maximiliano <maxil5gomez@gmail.com>
2022-09-29 16:49:01 -0300

    Agregado Header a mmsgcube

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+import { useRouter } from 'next/router'
 
 export default function Footer() {
+
+  const router = useRouter()
+
   return (
-    <div>Footer</div>
+   <footer className={styles.footer}>
+        <div className={`contenedor ${styles.contenido}`}>
+            <nav className={styles.navegacion}>
+            <Link href='/'>
+                        <a className={ router.pathname === '/' ? styles.active : '' } > Inicio</a>
+                    </Link>
+                    <Link href='/nosotros'>
+                        <a className={ router.pathname === '/nosotros' ? styles.active : '' }> Nosotros</a>
+                    </Link>
+                    <Link href='/blog'>
+                        <a className={ router.pathname === '/blog' ? styles.active : '' }> Blog</a>
+                    </Link>
+                    <Link href='/tienda'>
+                        <a className={ router.pathname === '/tienda' ? styles.active : '' }> Tienda</a>
+                    </Link>
+            </nav>
+            <p className={styles.copyright}>Todos los derechos reservados</p>
+    </div>
+    </footer>
   )
+  
 }
--- components/Header.js
+++ components/Header.js
@@ -5,34 +5,37 @@ import { useRouter } from 'next/router'
 import styles from "../styles/Header.module.css"
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-        <div className={`contenedor ${styles.barra}`} >
-            
-              <div>
-                <Link href='/'>
-                    <a>
-                        <Image width={400} height={100} src="/img/liogo2.svg" alt="Imagen Logo"/>
-                    </a>
-                  
-                </Link>
-              </div>
-              <nav className={styles.navegacion}>
-                  <Link href='/'>
-                    <a > Inicio</a>
-                  </Link>
-                  <Link href='/nosotros'>
-                    <a > Nosotros</a>
-                  </Link>
-                  <Link href='/blog'>
-                    <a > Blog</a>
-                  </Link>
-                  <Link href='/tienda'>
-                    <a > Tienda</a>
-                  </Link>
-              </nav>
-            
-        </div>
-    </header>
+  
+    const router = useRouter()
+
+    return (
+
+        <header className={styles.header}>
+            <div className={`contenedor ${styles.barra}`} >
+                
+                <div>
+                    <Link href='/'>
+                        <a>
+                            <Image width={400} height={100} src="/img/liogo2.svg" alt="Imagen Logo"/>
+                        </a>
+                    </Link>
+                </div>
+                <nav className={styles.navegacion}>
+                    <Link href='/'>
+                        <a className={ router.pathname === '/' ? styles.active : '' } > Inicio</a>
+                    </Link>
+                    <Link href='/nosotros'>
+                        <a className={ router.pathname === '/nosotros' ? styles.active : '' }> Nosotros</a>
+                    </Link>
+                    <Link href='/blog'>
+                        <a className={ router.pathname === '/blog' ? styles.active : '' }> Blog</a>
+                    </Link>
+                    <Link href='/tienda'>
+                        <a className={ router.pathname === '/tienda' ? styles.active : '' }> Tienda</a>
+                    </Link>
+                </nav>
+                
+            </div>
+        </header>
   )
 }
