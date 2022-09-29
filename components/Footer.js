d334267a99ddb5a9f7b644e7943647cb558b2962
maximiliano <maxil5gomez@gmail.com>
2022-09-29 16:51:50 -0300

    Agregado Footer a mmsgcube

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/Footer.module.css'
-import { useRouter } from 'next/router'
 
 export default function Footer() {
-
-  const router = useRouter()
-
   return (
    <footer className={styles.footer}>
         <div className={`contenedor ${styles.contenido}`}>
             <nav className={styles.navegacion}>
-            <Link href='/'>
-                        <a className={ router.pathname === '/' ? styles.active : '' } > Inicio</a>
-                    </Link>
-                    <Link href='/nosotros'>
-                        <a className={ router.pathname === '/nosotros' ? styles.active : '' }> Nosotros</a>
-                    </Link>
-                    <Link href='/blog'>
-                        <a className={ router.pathname === '/blog' ? styles.active : '' }> Blog</a>
-                    </Link>
-                    <Link href='/tienda'>
-                        <a className={ router.pathname === '/tienda' ? styles.active : '' }> Tienda</a>
-                    </Link>
+                <Link href='/'>Inicio</Link>
+                <Link href='/nosotros'>Nosotros</Link>
+                <Link href='/blog'>Blog</Link>
+                <Link href='/tienda'>Tienda</Link>
             </nav>
             <p className={styles.copyright}>Todos los derechos reservados</p>
     </div>
